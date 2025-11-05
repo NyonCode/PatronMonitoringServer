373b519c1fb9c1c20e637ee566bc889e5fffad22
SmithCZE <ondrejnyklicek@icloud.com>
2025-11-05 15:01:42 +0100

    Update

--- resources/views/livewire/customer/agent-log.blade.php
+++ resources/views/livewire/customer/agent-log.blade.php
@@ -30,8 +30,9 @@
         <div class="space-y-6 p-4">
 
             <div class="grid grid-cols-1 xl:grid-cols-2 gap-6">
-                <x-system.logs.agent-log :logs="$agentLog->agent_log" />
-                <x-system.logs.system-log :logs="$agentLog->system_logs" />
+
+{{--                <x-system.logs.agent-log :logs="$agentLog->agent_log" />
+                <x-system.logs.system-log :logs="$agentLog->system_logs" />--}}
             </div>
 
         </div>
