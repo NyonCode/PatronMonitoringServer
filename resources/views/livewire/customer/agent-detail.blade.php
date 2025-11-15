7b7d51c5923ac956ff8ddf3be5c55c123e12ce1b
ondrejnyklicek <ondrejnyklicek@icloud.com>
2025-11-15 20:51:44 +0100

    Update

--- resources/views/livewire/customer/agent-detail.blade.php
+++ resources/views/livewire/customer/agent-detail.blade.php
@@ -146,8 +146,21 @@
                         <div>
                             <p class="text-sm text-zinc-600 dark:text-zinc-400">Mapper drivers</p>
                             @dump($sessionInfo->mapped_drivers)
-                            <p class="font-mono text-zinc-900 dark:text-white"></p>
+                            @foreach($sessionInfo->mapped_drivers as $mappedDrivers)
+                                    <p class="font-mono text-zinc-900 dark:text-white">{{ $mappedDrivers->Letter }}</p>
+                            @endforeach
                         </div>
+
+                        <div>
+                            <p class="text-sm text-zinc-600 dark:text-zinc-400">Session time</p>
+                            @dump($sessionInfo->accessible_paths)
+                            @foreach($sessionInfo->accessible_paths as $accessiblePaths)
+                                <p class="font-mono text-zinc-900 dark:text-white">
+                                </p>
+
+                            @endforeach
+                        </div>
+
                     </div>
                 </div>
             @endif
