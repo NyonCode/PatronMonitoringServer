5d345359ea73406e3acfb50d1e477119a442a714
Ondřej Nyklíček <60318239+ONyklicek@users.noreply.github.com>
2025-11-27 13:00:03 +0100

    Add shutdown route for client in API

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,5 @@ Route::post('/clients/register', [AgentController::class, 'registerClient']);
 Route::middleware(AgentTokenMiddleware::class)->group(function () {
     Route::post('/clients/{UUID}/heartbeat', [AgentController::class, 'heartbeat']);
     Route::post('/clients/{UUID}/logs', [AgentController::class, 'logs']);
+    Route::post('/clients/{UUID}/shutdown', [AgentController::class, 'shutdown']);
 });
